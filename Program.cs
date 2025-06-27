3cf9bb4703b6f0f6b0f28e1bcc6e5b9bd56d23ad
ernaneco <ernani.mandarino@gmail.com>
2025-06-27 15:55:00 -0300

    Alteracao nos arquivos do desafio do Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456", "XYZ", "0000001", 16);
+Console.WriteLine($"O número do celular Nokia é {nokia.Numero}.");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tetris");
+
+Iphone iphone = new Iphone("456789", "ABC", "1111110", 32);
+Console.WriteLine($"O número do celular Iphone é {iphone.Numero}.");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Snake");
